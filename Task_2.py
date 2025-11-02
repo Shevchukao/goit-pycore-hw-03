e334d65fad02f6ebff0889a67eb35d9bc41e1dd7
Anton Shevchuk <62426412+Shevchukao@users.noreply.github.com>
2025-11-02 13:17:58 +0200

    Refactor get_numbers_ticket function conditions
    Updated function to remove quantity check in conditions.

--- Task_2.py
+++ Task_2.py
@@ -5,7 +5,7 @@ def get_numbers_ticket(
     min: int, max: int, quantity: int
 ) -> list:  # define function with three integer parameters
     unique_numbers = set()  # initialized empty set for unique elements
-    if (min >= 1) and (max <= 1000) and (min <= quantity <= max):  # check conditions
+    if (min >= 1) and (max <= 1000):  # check conditions
         while (
             len(unique_numbers) < quantity
         ):  # while loop that continues running until unique_numbers set contains required number of elements
@@ -21,3 +21,4 @@ lottery_numbers = get_numbers_ticket(
     1, 80, 10
 )  # variable assignment call function with valid argument
 print("Ваші лотерейні числа:", lottery_numbers)  # output returned list
+
