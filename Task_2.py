b40f863bfe3b24ac61e79a22a1f31731d81fc901
Anton Shevchuk <anton.shevchuk.1998@gmail.com>
2025-11-04 13:33:02 +0200

    repeat change if conditions

--- Task_2.py
+++ Task_2.py
@@ -5,9 +5,7 @@ def get_numbers_ticket(
     min: int, max: int, quantity: int
 ) -> list:  # define function with three integer parameters
     unique_numbers = set()  # initialized empty set for unique elements
-    if (
-        (1 <= min < max) and (min < max <= 1000) and ((max - min) + 1 >= quantity)
-    ):  # check conditions
+    if (1 <= min < max <= 1000) and ((max - min) + 1 >= quantity):  # check conditions
         #  while loop that continues running until unique_numbers set contains required number of elements
         while len(unique_numbers) < quantity:
             # insert into set new random numbers
@@ -18,5 +16,5 @@ def get_numbers_ticket(
 
 
 # variable assignment call function with valid argument
-lottery_numbers = get_numbers_ticket(10, 14, 6)
+lottery_numbers = get_numbers_ticket(10, 44, 6)
 print("Ваші лотерейні числа:", lottery_numbers)  # output returned list
