8801179831f26c80855a513e5abc3430c203c2a7
Anton Shevchuk <anton.shevchuk.1998@gmail.com>
2025-11-04 13:24:41 +0200

    Task_2 change if conditions

--- Task_2.py
+++ Task_2.py
@@ -5,21 +5,18 @@ def get_numbers_ticket(
     min: int, max: int, quantity: int
 ) -> list:  # define function with three integer parameters
     unique_numbers = set()  # initialized empty set for unique elements
-    if (min >= 1) and (max <= 1000):  # check conditions
-        while (
-            len(unique_numbers) < quantity
-        ):  # while loop that continues running until unique_numbers set contains required number of elements
-            unique_numbers.add(
-                random.randint(min, max)
-            )  # insert into set new random numbers
+    if (
+        (1 <= min < max) and (min < max <= 1000) and ((max - min) + 1 >= quantity)
+    ):  # check conditions
+        #  while loop that continues running until unique_numbers set contains required number of elements
+        while len(unique_numbers) < quantity:
+            # insert into set new random numbers
+            unique_numbers.add(random.randint(min, max))
         return sorted(list(unique_numbers))  # return sorted list from set
     else:
         return list(unique_numbers)  # return empty list
 
 
-lottery_numbers = get_numbers_ticket(
-    10, 20, 6
-)  # variable assignment call function with valid argument
+# variable assignment call function with valid argument
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа:", lottery_numbers)  # output returned list
-
-
